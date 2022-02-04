fa5c0d30c73e1887aca68012c7a13686cf9dd7a7
Anthony Limani <alimani9193@gmail.com>
2022-02-04 10:48:30 -0500

    Added project Lombok to reduce boilerplate code

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -1,9 +1,13 @@
 package guru.springframework.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
 @Entity
+@EqualsAndHashCode(exclude = {"recipe"})
 public class Ingredient {
 
     @Id
@@ -19,49 +23,11 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
+    public Ingredient() {}
+
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure unit) {
         this.description = description;
         this.amount = amount;
         this.unit = unit;
-    }
-
-    public UnitOfMeasure getUnit() {
-        return unit;
-    }
-
-    public void setUnit(UnitOfMeasure unit) {
-        this.unit = unit;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
     }
 }
